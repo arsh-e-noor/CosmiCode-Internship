7ee1c2cb22b647ed165aa8d85b949d1d57470e6d
arsh-e-noor <arshkhan4877@gmail.com>
2025-07-31 17:45:55 -0700

    Fixing errors

--- Week2_VersionControl/File1.py
+++ Week2_VersionControl/File1.py
@@ -1,11 +1,14 @@
 def greet(name):
     return "Hello, " + name + "!"
 
+
 def add(a, b):
     return a + b
 
+
 def mul(a, b):
     return a * b
+
 
 if __name__ == "__main__":
     name = input("Enter your name: ")
@@ -15,7 +18,7 @@ if __name__ == "__main__":
         x = int(input("Enter first number: "))
         y = int(input("Enter second number: "))
         print("Sum:", add(x, y))
-        print("Multiply", mul(x, y))
+        print("Multiply:", mul(x, y))
 
-    except:
+    except ValueError:
         print("Please enter valid numbers.")
